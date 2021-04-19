3c0e95ee45d42e8b2a6df8cf62aaa470cd4272c3
Steve Jackson <steve@testdouble.com>
2021-04-18 21:26:42 -0500

    Pull balance info from GCS

--- phoenix/budgets/lib/budgets/text_file_loader.ex
+++ phoenix/budgets/lib/budgets/text_file_loader.ex
@@ -1,6 +1,27 @@
 defmodule Budgets.TextFileLoader do
   def load_balances do
-    load_balances("balances.txt")
+    download_file
+    |> load_balances
+  end
+
+  def download_file do
+    bucket = "jackson-budget-1519498484201-uploads"
+    key = "balances.txt"
+    {:ok, token} = Goth.fetch(Budgets.Goth)
+    conn = GoogleApi.Storage.V1.Connection.new(token.token)
+    # alt:media gives us contents, otherwise nada
+    case GoogleApi.Storage.V1.Api.Objects.storage_objects_get(conn, bucket, key, alt: "media") do
+      {:error, %Tesla.Env{body: <<"No such object:", _rest::binary>>}} -> {:error, :file_not_found}
+      {:ok, %Tesla.Env{body: body}} -> {:ok, body}
+    end
+  end
+
+  def load_balances({:ok, body}) do
+    body
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(&blank?/1)
+    |> Enum.map(&parse_line/1)
   end
 
   def load_balances(filename) do
@@ -17,11 +38,24 @@ defmodule Budgets.TextFileLoader do
     to_balance([category, 0, balance])
   end
 
+  defp to_balance([category]) do
+    to_balance([category, 0])
+  end
+
   defp to_balance([category, budget, balance]) do
      %{
        category: category,
        budget: budget,
        balance: balance
      }
+  end
+
+  defp blank?(str) do
+    case str do
+      nil -> true
+      "" -> true
+      " " <> r -> blank?(r)
+      _ -> false
+    end
   end
 end
